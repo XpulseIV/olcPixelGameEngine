206534cca05ffc960b4fe6c31ebecfe2df947f43
Moros1138 <moros1138@gmail.com>
2023-12-05 21:58:06 -0500

    is it ascii or special?

--- olcPixelGameEngine.h
+++ olcPixelGameEngine.h
@@ -6186,7 +6186,7 @@ namespace olc {
 					default:
 						break;
 				}
-				std::cout << "PRESSED: " << (int)key << "\n";
+				std::cout << "PRESSED ASCII: " << (int)key << "\n";
 				ptrPGE->olc_UpdateKeyState(mapKeys[key], false);
 			});
 
@@ -6199,7 +6199,7 @@ namespace olc {
 			// Handle KeyRelease of the "special" codes
 			glutSpecialUpFunc([](int key, int x, int y) -> void
 			{
-				std::cout << "PRESSED: " << (int)key << "\n";
+				std::cout << "PRESSED SPECIAL: " << (int)key << "\n";
 				ptrPGE->olc_UpdateKeyState(mapKeys[key | specialFlag], false);
 			});
 
